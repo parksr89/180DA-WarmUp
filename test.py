4a42708f49fad03ec0c3945e7841d8346ad00adf
parksr89 <54193969+parksr89@users.noreply.github.com>
2022-10-02 18:17:22 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    x = "ECE_180 _DA_DB"
+    if x == "EE_180_DA_DB":
+        print("You are Living in 2022")
+    else:
+        # this is a comment
+        x = x + " - Best class ever"
+        print(x)
